1fb06077c88f8b5f9079801ff641c3ae7e92a104
Rob Napier <rob_napier@kace.com>
2013-07-19 16:24:53 -0400

    Add LayerAnimation

---
+++ ch08-Animation/LayerAnimation/LayerAnimation/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LayerAnimation
+//
+//  Created by Rob Napier on 7/17/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "LayerAnimationAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([LayerAnimationAppDelegate class]));
+  }
+}
