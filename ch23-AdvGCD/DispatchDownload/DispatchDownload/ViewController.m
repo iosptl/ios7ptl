2ce4a116b03b59fed6c2c99fe42aa609d5b077b3
Rob Napier <robnapier@gmail.com>
2013-10-04 16:18:29 -0400

    Good, simple version of DispatchDownload, factored well.

--- ch23-AdvGCD/DispatchDownload/DispatchDownload/ViewController.m
+++ ch23-AdvGCD/DispatchDownload/DispatchDownload/ViewController.m
@@ -14,19 +14,10 @@
 #include <netinet/in.h>
 #include <netdb.h>
 
-NSString * const kDownloadURLString = @"http://upload.wikimedia.org/wikipedia/commons/9/97/The_Earth_seen_from_Apollo_17.jpg";
-const char * kHost = "upload.wikimedia.org";
-const int kPort = 80;
-NSString * const kPath = @"/wikipedia/commons/9/97/The_Earth_seen_from_Apollo_17.jpg";
-
-@interface ViewController ()
-
-@end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-  [super viewDidLoad];
+- (dispatch_fd_t)connectToHostName:(NSString *)hostName port:(int)port {
   int s;
   struct sockaddr_in sa;
   struct hostent *he;
@@ -38,9 +29,9 @@ NSString * const kPath = @"/wikipedia/commons/9/97/The_Earth_seen_from_Apollo_17
   bzero(&sa, sizeof sa);
 
   sa.sin_family = AF_INET;
-  sa.sin_port = htons(kPort);
+  sa.sin_port = htons(port);
 
-  if ((he = gethostbyname(kHost)) == NULL) {
+  if ((he = gethostbyname([hostName UTF8String])) == NULL) {
     NSAssert(NO, @"gethostbyname failure:%d", errno);
   }
 
@@ -51,45 +42,117 @@ NSString * const kPath = @"/wikipedia/commons/9/97/The_Earth_seen_from_Apollo_17
     NSAssert(NO, @"connect failed:%d", errno);
   }
 
-  dispatch_io_t serverChannel = dispatch_io_create(DISPATCH_IO_STREAM, s, dispatch_get_main_queue(), ^(int error){
-    close(s);
-  });
+  return s;
+}
 
-  NSString *getString = [NSString stringWithFormat:@"GET %@ HTTP/1.1\r\nHost: %s\r\n\r\n", kPath, kHost];
+- (NSString *)outputFilePathForPath:(NSString *)path {
+  return [[NSTemporaryDirectory() stringByAppendingPathComponent:[path lastPathComponent]] stringByAppendingPathExtension:@"out"];
+}
+
+- (void)writeToChannel:(dispatch_io_t)channel
+                  data:(dispatch_data_t)writeData
+                 queue:(dispatch_queue_t)queue {
+  dispatch_io_write(channel, 0, writeData, queue,
+                    ^(bool done, dispatch_data_t remainingData, int error) {
+                      NSAssert(!error, @"File write error:%d", error);
+                      size_t unwrittenDataLength = 0;
+                      if (remainingData) {
+                        unwrittenDataLength = dispatch_data_get_size(remainingData);
+                      }
+                      NSLog(@"Wrote %zu bytes",
+                            dispatch_data_get_size(writeData) - unwrittenDataLength);
+                    });
+
+}
+
+- (void)readFromChannel:(dispatch_io_t)readChannel
+         writeToChannel:(dispatch_io_t)writeChannel
+                  queue:(dispatch_queue_t)queue {
+  dispatch_io_read(readChannel, 0, SIZE_MAX, queue,
+                   ^(bool serverReadDone,
+                     dispatch_data_t serverReadData,
+                     int serverReadError) {
+
+                     NSAssert(!serverReadError,
+                              @"Server read error:%d", serverReadError);
+                     if (serverReadDone) {
+                       NSLog(@"Done Downloading");
+                       dispatch_io_close(writeChannel, 0);
+                       dispatch_io_close(readChannel, 0);
+                     }
+                     else {
+                       [self writeToChannel:writeChannel
+                                       data:serverReadData
+                                      queue:queue];
+                     }
+                   });
+}
+
+- (dispatch_data_t)requestDataForHostName:(NSString *)hostName
+                                     path:(NSString *)path {
+  NSString *
+  getString = [NSString stringWithFormat:@"GET %@ HTTP/1.1\r\nHost: %@\r\n\r\n",
+               path, hostName];
   NSLog(@"Request:\n%@", getString);
+
   NSData *getData = [getString dataUsingEncoding:NSUTF8StringEncoding];
+  return dispatch_data_create(getData.bytes,
+                              getData.length,
+                              NULL,
+                              DISPATCH_DATA_DESTRUCTOR_DEFAULT);
+}
 
-  dispatch_data_t data = dispatch_data_create(getData.bytes, getData.length, NULL, DISPATCH_DATA_DESTRUCTOR_DEFAULT);
+- (void)HTTPDownloadContentsFromHostName:(NSString *)hostName
+                                    port:(int)port
+                                    path:(NSString *)path {
 
-  NSString *writePath = [[NSTemporaryDirectory() stringByAppendingPathComponent:[kPath lastPathComponent]] stringByAppendingPathExtension:@"out"];
+  dispatch_queue_t queue = dispatch_get_main_queue();
+
+  dispatch_fd_t socket = [self connectToHostName:hostName port:port];
+
+  dispatch_io_t
+  serverChannel = dispatch_io_create(DISPATCH_IO_STREAM, socket, queue,
+                                     ^(int error) {
+                                       NSAssert(!error,
+                                                @"Failed socket:%d", error);
+                                       NSLog(@"Closing connection");
+                                       close(socket);
+                                     });
+
+  dispatch_data_t requestData = [self requestDataForHostName:hostName path:path];
+
+  NSString *writePath = [self outputFilePathForPath:path];
   NSLog(@"Writing to %@", writePath);
-  dispatch_io_t fileChannel = dispatch_io_create_with_path(DISPATCH_IO_STREAM, [writePath UTF8String], O_WRONLY|O_CREAT|O_TRUNC, S_IRWXU, dispatch_get_main_queue(), nil);
 
-  dispatch_queue_t queue = dispatch_queue_create("DispatchDownload", DISPATCH_QUEUE_CONCURRENT);
+  dispatch_io_t
+  fileChannel = dispatch_io_create_with_path(DISPATCH_IO_STREAM,
+                                             [writePath UTF8String],
+                                             O_WRONLY|O_CREAT|O_TRUNC,
+                                             S_IRWXU,
+                                             queue,
+                                             nil);
 
-  dispatch_io_write(serverChannel, 0, data, queue, ^(bool serverWriteDone, dispatch_data_t serverWriteData, int serverWriteError) {
-    NSAssert(serverWriteError == 0, @"Server write error:%d", serverWriteError);
-    if (serverWriteDone) {
-      dispatch_io_read(serverChannel, 0, SIZE_MAX, queue, ^(bool serverReadDone, dispatch_data_t serverReadData, int serverReadError) {
-        NSAssert(serverReadError == 0, @"Server read error:%d", serverReadError);
-        if (serverReadDone) {
-          NSLog(@"Done");
-          dispatch_io_close(fileChannel, 0);
-          dispatch_io_close(serverChannel, 0);
-        }
-        else {
-          dispatch_io_write(fileChannel, 0, serverReadData, queue, ^(bool fileWriteDone, dispatch_data_t fileWriteRemainingData, int fileWriteError) {
-            NSAssert(fileWriteError == 0, @"File write error:%d", fileWriteError);
-            size_t unwrittenDataLength = 0;
-            if (fileWriteRemainingData) {
-              unwrittenDataLength = dispatch_data_get_size(fileWriteRemainingData);
-            }
-            NSLog(@"Wrote %zu bytes", dispatch_data_get_size(serverReadData) - unwrittenDataLength);
-          });
-        }
-      });
-    }
-  });
+  dispatch_io_write(serverChannel, 0, requestData, queue,
+                    ^(bool serverWriteDone,
+                      dispatch_data_t serverWriteData,
+                      int serverWriteError) {
+
+                      NSAssert(!serverWriteError,
+                               @"Server write error:%d", serverWriteError);
+                      if (serverWriteDone) {
+                        [self readFromChannel:serverChannel
+                               writeToChannel:fileChannel
+                                        queue:queue];
+                      }
+                    });
+}
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+
+  [self HTTPDownloadContentsFromHostName:@"upload.wikimedia.org"
+                                    port:80
+                                    path:@"/wikipedia/commons/9/97/The_Earth_seen_from_Apollo_17.jpg"];
 }
 
 @end
