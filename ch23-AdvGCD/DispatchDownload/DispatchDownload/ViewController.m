c0013a5e80557b3ae8deca40da7331981219e99b
Rob Napier <robnapier@gmail.com>
2013-10-04 10:34:15 -0400

    Simplified DispatchDownload

--- ch23-AdvGCD/DispatchDownload/DispatchDownload/ViewController.m
+++ ch23-AdvGCD/DispatchDownload/DispatchDownload/ViewController.m
@@ -25,8 +25,7 @@ NSString * const kPath = @"/wikipedia/commons/9/97/The_Earth_seen_from_Apollo_17
 
 @implementation ViewController
 
-- (void)viewDidLoad
-{
+- (void)viewDidLoad {
   [super viewDidLoad];
   int s;
   struct sockaddr_in sa;
@@ -57,89 +56,40 @@ NSString * const kPath = @"/wikipedia/commons/9/97/The_Earth_seen_from_Apollo_17
   });
 
   NSString *getString = [NSString stringWithFormat:@"GET %@ HTTP/1.1\r\nHost: %s\r\n\r\n", kPath, kHost];
-  NSLog(@"%@", getString);
+  NSLog(@"Request:\n%@", getString);
   NSData *getData = [getString dataUsingEncoding:NSUTF8StringEncoding];
 
   dispatch_data_t data = dispatch_data_create(getData.bytes, getData.length, NULL, DISPATCH_DATA_DESTRUCTOR_DEFAULT);
 
-  NSString *writePath = [NSTemporaryDirectory() stringByAppendingPathComponent:[kPath lastPathComponent]];
+  NSString *writePath = [[NSTemporaryDirectory() stringByAppendingPathComponent:[kPath lastPathComponent]] stringByAppendingPathExtension:@"out"];
+  NSLog(@"Writing to %@", writePath);
   dispatch_io_t fileChannel = dispatch_io_create_with_path(DISPATCH_IO_STREAM, [writePath UTF8String], O_WRONLY|O_CREAT|O_TRUNC, S_IRWXU, dispatch_get_main_queue(), nil);
 
-  dispatch_io_write(serverChannel, 0, data, dispatch_get_global_queue(0, 0), ^(bool done, dispatch_data_t data, int error) {
-    if (done) {
-      [self readFromChannel:serverChannel toChannel:fileChannel];
-    }
-  });
-}
+  dispatch_queue_t queue = dispatch_queue_create("DispatchDownload", DISPATCH_QUEUE_CONCURRENT);
 
-- (void)readFromChannel:(dispatch_io_t)readChannel toChannel:(dispatch_io_t)writeChannel {
-  dispatch_io_set_high_water(readChannel, 100);
-  __block BOOL haveHeader = NO;
-  __block NSData *lastLine;
-  dispatch_io_read(readChannel, 0, SIZE_MAX, dispatch_get_global_queue(0, 0), ^(bool done, dispatch_data_t data, int error) {
-    if (!haveHeader) {
-      dispatch_data_apply(data, ^bool(dispatch_data_t region, size_t offset, const void *buffer, size_t size) {
-        printf("size:%zu\n", size);
-        NSData *line;
-        if (lastLine) {
-          NSLog(@"temporary line");
-          NSMutableData *newData = [lastLine mutableCopy];
-          [newData appendBytes:buffer length:size];
-          line = newData;
+  dispatch_io_write(serverChannel, 0, data, queue, ^(bool serverWriteDone, dispatch_data_t serverWriteData, int serverWriteError) {
+    NSAssert(serverWriteError == 0, @"Server write error:%d", serverWriteError);
+    if (serverWriteDone) {
+      dispatch_io_read(serverChannel, 0, SIZE_MAX, queue, ^(bool serverReadDone, dispatch_data_t serverReadData, int serverReadError) {
+        NSAssert(serverReadError == 0, @"Server read error:%d", serverReadError);
+        if (serverReadDone) {
+          NSLog(@"Done");
+          dispatch_io_close(fileChannel, 0);
+          dispatch_io_close(serverChannel, 0);
         }
         else {
-          line = [[NSData alloc] initWithBytesNoCopy:(void*)buffer length:size freeWhenDone:NO];
+          dispatch_io_write(fileChannel, 0, serverReadData, queue, ^(bool fileWriteDone, dispatch_data_t fileWriteRemainingData, int fileWriteError) {
+            NSAssert(fileWriteError == 0, @"File write error:%d", fileWriteError);
+            size_t unwrittenDataLength = 0;
+            if (fileWriteRemainingData) {
+              unwrittenDataLength = dispatch_data_get_size(fileWriteRemainingData);
+            }
+            NSLog(@"Wrote %zu bytes", dispatch_data_get_size(serverReadData) - unwrittenDataLength);
+          });
         }
-
-        NSLog(@"%@", [[NSString alloc] initWithData:line encoding:NSUTF8StringEncoding]);
-
-        NSRange range = [line rangeOfData:[@"\r\n\r\n" dataUsingEncoding:NSUTF8StringEncoding] options:0 range:NSMakeRange(0, [line length])];
-        if (range.location == NSNotFound ) {
-          char lastChar = ((char*)buffer)[size - 1];
-          if( lastChar == '\r' || lastChar == '\n') {
-            lastLine = line;
-          }
-        }
-        else {
-          haveHeader = YES;
-          dispatch_io_set_high_water(readChannel, SIZE_MAX);
-          // WRONG if reading the newline
-          dispatch_data_t writeData = dispatch_data_create_subrange(region, NSMaxRange(range), [line length] - NSMaxRange(range));
-          [self writeData:writeData toChannel:writeChannel];
-        }
-        return true;
       });
     }
-    else {
-      [self writeData:data toChannel:writeChannel];
-    }
   });
-}
-
-- (void)writeData:(dispatch_data_t)data toChannel:(dispatch_io_t)channel {
-  dispatch_io_write(channel, 0, data, dispatch_get_global_queue(0, 0), ^(bool done, dispatch_data_t data, int error){});
-}
-
-//
-//            header = dispatch_data_create_concat(header, region);
-//            const void *buffer;
-//            size_t size;
-//            dispatch_data_create_map(header, &buffer, &size);
-//            if (memchr(<#const void *#>, <#int#>, <#size_t#>))
-//
-//            printf("--- %zu ---\n", size);
-//          write(STDOUT_FILENO, buffer,size);
-//          exit(0);
-
-
-// Do any additional setup after loading the view, typically from a nib.
-
-
-
-- (void)didReceiveMemoryWarning
-{
-  [super didReceiveMemoryWarning];
-  // Dispose of any resources that can be recreated.
 }
 
 @end
